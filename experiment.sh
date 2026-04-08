6103aca5e68a9f59e0a87f6739206059bd9fb2a8
Diego Ornelas <dkdiego505@unm.edu>
2026-04-08 14:58:36 -0600

    Save experiment results to logfile

--- experiment.sh
+++ experiment.sh
@@ -8,13 +8,18 @@ threshold=30.0
 max=$(nproc --all)
 threadAmounts=(1 $(seq 2 2 $max))
 
-# Set OpenMP env variables to disable hyper-threading (only 1 thread per core)
+# Set OpenMP env. variables to disable hyper-threading (only 1 thread per core)
 export OMP_PROC_BIND=true
 export OMP_PLACES=cores
 
-echo "Experiment: running simulation with OpenMP multi-threading up to $max threads"
-echo "Note: all timestamps are in seconds since Epoch, with nanosecond floating-point precision"
-echo ""
+# Initialize log file w/ current timestamp to guarantee unique filename
+logfile="Experiment_$(date +"%Y%m%d-%H%M%S").log"
+echo "Experiment: running simulation with OpenMP multi-threading up to $max threads" > "$logfile"
+echo "Note: all timestamps are in seconds since Epoch, with nanosecond floating-point precision" >> "$logfile"
+echo "" >> "$logfile"
+
+# Inform terminal via stdout that experiment execution is under way
+echo "[$(date +"%D %T")] Experiment in progress..."
 
 # Main experiment loop: test simulation runtimes at all thread amounts
 for n in "${threadAmounts[@]}"; do
@@ -23,8 +28,8 @@ for n in "${threadAmounts[@]}"; do
 
     # Inner loop: repeat sim multiple times at each thread amount to determine average runtimes
     for ((i=1; i<=20; i++)); do
-        # Report thread amount & iteration number to stdout
-        echo "Threads: $n Iteration: $i"
+        # Report thread amount & iteration number to log file
+        echo "Threads: $n Iteration: $i" >> "$logfile"
 
         # TODO: check CPU temp & hold until threshold temp reached
         # TODO: suspend all other user processes on the system
@@ -34,13 +39,18 @@ for n in "${threadAmounts[@]}"; do
         eval $program
         end=$(date +"%s.%N")
 
-        # Report recorded start & end times to stdout
-        echo "Simulation Start: $start"
-        echo "Simulation End: $end"
-        echo ""
+        # Report recorded start & end times to log file
+        echo "Simulation Start: $start" >> "$logfile"
+        echo "Simulation End: $end" >> "$logfile"
+        echo "" >> "$logfile"
     done
 done
 
-# Add verification of full experiment run to stdout (if this is missing, script was interrupted)
-echo "Experiment Completion: YES"
+# Add verification of full experiment run to log file (if this is missing, script was interrupted)
+echo "Experiment Completion: YES" >> "$logfile"
+
+# Inform terminal via stdout that experiment finished & where to find log file
+echo "[$(date +"%D %T")] ...Experiment complete."
+echo "[$(date +"%D %T")] Results saved: $logfile"
+
 exit 0
